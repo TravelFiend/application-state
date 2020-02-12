105c2696c6d451de5819790cc221c509414916ab
TravelFiend <mikegrace274@gmail.com>
2020-02-12 13:58:29 -0800

    actions written

---
+++ src/actions/buttonIncrementors.js
@@ -0,0 +1,19 @@
+export const INCREMENT_DRINK_COFFEE = 'INCREMENT_DRINK_COFFEE';
+export const incrementDrinkCoffee = () => ({
+  type: INCREMENT_DRINK_COFFEE
+});
+
+export const INCREMENT_EAT_SNACK = 'INCREMENT_EAT_SNACK';
+export const incrementEatSnack = () => ({
+  type: INCREMENT_EAT_SNACK
+});
+
+export const INCREMENT_TAKE_NAP = 'INCREMENT_TAKE_NAP';
+export const incrementTakeNap = () => ({
+  type: INCREMENT_TAKE_NAP
+});
+
+export const INCREMENT_STUDY = 'INCREMENT_STUDY';
+export const incrementStudy = () => ({
+  type: INCREMENT_STUDY
+});
