3a7521e9d24eff150ade817be7603e3a30634f2c
TravelFiend <mikegrace274@gmail.com>
2020-02-12 15:35:00 -0800

    actions test written and passing

--- src/actions/incrementors.test.js
+++ src/actions/incrementors.test.js
@@ -15,4 +15,28 @@ describe('actions', () => {
 
     expect(action).toEqual({ type: INCREMENT_COFFEES });
   });
+
+  it('creates an incrementSnacks action', () => {
+    const action = incrementSnacks();
+
+    expect(action).toEqual({
+      type: INCREMENT_SNACKS
+    });
+  });
+
+  it('creates an incrementNaps action', () => {
+    const action = incrementNaps();
+
+    expect(action).toEqual({
+      type: INCREMENT_NAPS
+    });
+  });
+
+  it('creates an incrementCoffees action', () => {
+    const action = incrementCoffees();
+
+    expect(action).toEqual({
+      type: INCREMENT_COFFEES
+    });
+  });
 });
